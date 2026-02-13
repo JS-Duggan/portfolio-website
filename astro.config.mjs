f53f3dae482d361adff35eae0dc1c0a3abfbdbe9
Jack Duggan <jacksdggn@gmail.com>
2026-02-13 23:21:32 +1030

    feat: config for Git Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com/",
+  site: "https://js-duggan.github.io",
+  base: "/portfolio-website",
 
   vite: {
     plugins: [tailwindcss()]
